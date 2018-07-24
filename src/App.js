406e44c1c4aa249b336e03697512b29644a86598
Puria Nafisi Azizi <puria@dyne.org>
2018-07-24 21:03:25 +0200

    :sparkles: Connection to the server
    Connecting to the validana infrastruture... for test purposes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Client, PrivateKey} from 'validana-client';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.privateKey = PrivateKey.generate()
+    this.client = Client.get()
+    this.state = {
+      prefix: 't_',
+      server: 'ws://localhost:8080'
+    }
+
+    this.connect = this.connect.bind(this);
+  }
+
+  connect() {
+    this.client.init(this.state.prefix, this.state.server)
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br/>
+        <label htmlFor="prefix">Prefix (VPROC_SIGNPREFIX)  </label>
+        <input type="text" name="prefix" defaultValue={this.state.prefix} />
+        <br/><br/>
+        <label htmlFor="server">WS url server   </label>
+        <input type="text" name="server" defaultValue={this.state.server} />
+        <br/><br/>
+        <button onClick={this.connect}>Connect</button>
       </div>
     );
   }
